e75c7770ea39b9abbec32a253122edd9486d9a15
Dan Parker <danparker55@gmail.comgit config --global user.name Dan>
2018-06-14 12:54:24 -0700

    Completed react intro challenge - Dan Parker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
+const user = {
+    firstName: "John",
+    lastName: "Smith"
+}
+
+function fullName({firstName, lastName}){
+    return `${firstName} ${lastName}`;
+}
+
+const greeting = <h1>Hello {fullName(user)}!</h1>;
+
+
 ReactDOM.render(
-    <App />,
+    greeting,
     document.getElementById('root')
 );
