cfb9aa6bf7425983d007a6585d6570076d7ac802
Dan Parker <danparker55@gmail.comgit config --global user.name Dan>
2018-06-14 16:09:23 -0700

    completed functional component demo - Dan P

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
+import Greeting from './greeting';
 
 const App = () => (
     <div>
         <div className="app">
             <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
+            {/* <h1>Welcome to React</h1> */}
+            <Greeting name='Dan' age = '26' favColor = 'blue'/>
+            <Greeting name="John Smith" favColor = 'red'/>
         </div>
     </div>
 );
