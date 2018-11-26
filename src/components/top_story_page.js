f039b3af26cb35a8bd903408d4914309688408d6
Dan Parker <danparker55@gmail.comgit config --global user.name Dan>
2018-11-26 07:36:27 -0800

    implemented styled components and fixed image error

--- src/components/top_story_page.js
+++ src/components/top_story_page.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import newspaper from '../assets/images/newspaper.jpg'
 
 const ROOT_URL = "https://api.nytimes.com/svc/topstories/v2"
 const API_KEY = "e8c448d58e55459b80b909578b959737"
+
 
 
 class TopStories extends Component {
@@ -18,7 +20,6 @@ class TopStories extends Component {
         const response = await axios.get(`${ROOT_URL}/${term}.json?api-key=${API_KEY}`)
         const data = response.data.results;
         this.setState({ data })
-        console.log('data', data)
     }
 
     navigateBack(){
@@ -26,23 +27,21 @@ class TopStories extends Component {
     }
 
     render() {
-        console.log('PROPS', this.props)
-
         if (!this.state.data[0]) {
             return <h1>loading...</h1>
         }
 
         const headlines = this.state.data.map((story, index) => {
+        
             return (
                 <div key={index}>
                     <div className="headline-content">
-                        <img src={story.multimedia[0].url} alt=""/>
+                        <img src={story.multimedia[0] ? story.multimedia[0].url : newspaper} />
                         <a target="_blank" href={story.url}><h4>{story.title}</h4></a>
                         <h5>- {story.byline}</h5>
                     </div>
                     <hr />
                 </div>
-
             )
         })
 
